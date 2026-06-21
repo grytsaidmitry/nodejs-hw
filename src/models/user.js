8601234e20b416cbba0afaef9c26091994bde46d
grytsaidmitry <dimasicus2008@gmail.com>
2026-06-21 18:01:57 +0200

    Add files via upload

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: { type: String, trim: true },
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true, min: 8 },
+    avatar: {
+      type: String,
+      required: false,
+      default: 'https://ac.goit.global/fullstack/react/default-avatar.jpg',
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+userSchema.pre('save', function (next) {
+  if (!this.username) {
+    this.username = this.email;
+  }
+
+  next();
+});
+
+export const User = model('User', userSchema);
